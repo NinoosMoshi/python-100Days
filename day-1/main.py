b30ddf2479e061e76a2b6a32c4e36460e348a6ab
NinoosMoshi <nalkaton@gmail.com>
2023-05-14 10:51:11 -0500

    day1 project

---
+++ day-1/main.py
@@ -0,0 +1,13 @@
+# print method
+print("hello world!")  # hello world!
+
+# print with input
+print("hello " + input("what is your name? "))  # hello ninos
+
+# length of string
+print(len(input("What is your name? ")))  # 5
+
+# variable
+
+name = input("what is your name")
+print(name)
---
+++ day-1/project.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("Which city did you frow up in?\n")
+
+pet = input("What is yhe name of a pet?\n")
+
+print("Your band name could be " + city + " " + pet)
